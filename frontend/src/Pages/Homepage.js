13cb3b4ac367cbdffcb930a4eb582e26c9f32f3a
abhi360g <abhi360g@gmail.com>
2023-02-13 22:08:18 -0500

    Done with homepage

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,7 +1,59 @@
+import {
+  Box,
+  Container,
+  Text,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
 import React from "react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <Container maxW="xl">
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+        textAlign="center"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans">
+          Lets chat
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>
+                <Login />
+              </p>
+            </TabPanel>
+            <TabPanel>
+              <p>
+                <p>
+                  <Signup />
+                </p>
+              </p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default Homepage;
