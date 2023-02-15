2bb86980b60ef4b320726bb88b961d5720d0822f
abhi360g <abhi360g@gmail.com>
2023-02-14 22:50:29 -0500

    Connected to MongoDB Atlas and working on login authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userModel = mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     profilePicture: {
       type: String,
-      required: true,
       default:
         "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
@@ -15,6 +15,20 @@ const userModel = mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.Model("User", userModel);
+userModel.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-module.export = User;
+userModel.pre("save", async function (next) {
+  //pre and post hooks
+  if (!this.isModified) {
+    next(); //middleware
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userModel);
+
+module.exports = User;
--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -6,6 +6,7 @@ import {
   InputGroup,
   InputRightElement,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 
@@ -16,10 +17,59 @@ const Signup = () => {
   const [password, setPassword] = useState();
   const [pic, setPic] = useState();
   const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
 
   const handleClick = () => setShow(!show);
 
-  const postDetails = (pics) => {};
+  const postDetails = (pics) => {
+    setLoading(true);
+    if (pics === undefined) {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+
+      return;
+    }
+
+    if (
+      pics.type === "image/jpg" ||
+      pics.type === "image/jpeg" ||
+      pics.type === "image/png"
+    ) {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "lets-chat");
+      data.append("cloud_name", "abhi-image-cloud");
+      fetch("https://api.cloudinary.com/v1_1/abhi-image-cloud/image/upload", {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPic(data.url.toString());
+          console.log(data.url.toString());
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setLoading(false);
+        });
+    } else {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+  };
 
   const submitHandler = (pics) => {};
 
@@ -86,6 +136,7 @@ const Signup = () => {
         width="100%"
         style={{ marginTop: 15 }}
         onClick={submitHandler}
+        isLoading={loading}
       >
         Sign Up
       </Button>
